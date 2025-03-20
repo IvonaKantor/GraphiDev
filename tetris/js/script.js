92f730b92edf1bbfdb9088a59a5033a997c952b5
ivona <ik87056@student.uwb.edu.pl>
2025-03-20 19:10:09 +0100

    init field of game and const variables

--- tetris/js/script.js
+++ tetris/js/script.js
@@ -1,4 +1,13 @@
 const canvas = document.getElementById('game');
 const context = canvas.getContext('2d');
-const grid = 32;
+const blockSize = 32;
 const field = [];
+const figures = {
+    'I': [[0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0], [0, 0, 0, 0]],
+    'J': [[1, 0, 0], [1, 1, 1], [0, 0, 0]],
+    'L': [[0, 0, 1], [1, 1, 1], [0, 0, 0]],
+    'O': [[1, 1], [1, 1]],
+    'S': [[0, 1, 1], [1, 1, 0], [0, 0, 0]],
+    'Z': [[1, 1, 0], [0, 1, 1], [0, 0, 0]],
+    'T': [[0, 1, 0], [1, 1, 1], [0, 0, 0]]
+};
