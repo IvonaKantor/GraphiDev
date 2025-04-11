8c2badc72903da3218e8358c55bfc094c5e28f30
ivona <ik87056@student.uwb.edu.pl>
2025-04-12 01:33:56 +0200

    added image loading

--- photo_editor/src/main/java/org/example/Main.java
+++ photo_editor/src/main/java/org/example/Main.java
@@ -7,6 +7,16 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            File inputFile = new File("image.png");
+            BufferedImage image = ImageIO.read(inputFile);
 
+            if (image == null) {
+                System.out.println("No image found");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("An error occurred while loading or saving an image: " + e.getMessage());
+        }
     }
 }
