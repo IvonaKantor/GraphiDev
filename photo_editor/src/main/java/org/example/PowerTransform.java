b1daa064a9b980d7822a9c3a3f507a44fd76c065
ivona <ik87056@student.uwb.edu.pl>
2025-04-12 13:27:06 +0200

    created a new color and set it for result image

--- photo_editor/src/main/java/org/example/PowerTransform.java
+++ photo_editor/src/main/java/org/example/PowerTransform.java
@@ -1,28 +1,30 @@
 package org.example;
+
 import java.awt.image.BufferedImage;
 
 public class PowerTransform {
     public BufferedImage brighten(BufferedImage inputImg, double amount) {
 
-        if(inputImg == null)
+        if (inputImg == null)
             return null;
 
         int width = inputImg.getWidth();
         int height = inputImg.getHeight();
         BufferedImage outputImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
-        for(int y = 0; y < height; y++) {
-            for(int x = 0; x < width; x++) {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
                 int rgb = inputImg.getRGB(x, y);
                 int alpha = (rgb >> 24) & 0xff;
-                int red = (int)Math.min(255,((rgb >> 16) & 0xff) * amount);
-                int green = (int)Math.min(255,((rgb >> 8) & 0xff)* amount);
-                int blue = (int)Math.min(255, (rgb & 0xff) * amount);
+                int red = (int) Math.min(255, ((rgb >> 16) & 0xff) * amount);
+                int green = (int) Math.min(255, ((rgb >> 8) & 0xff) * amount);
+                int blue = (int) Math.min(255, (rgb & 0xff) * amount);
+
+                int newColor = (alpha << 24) | (red << 16) | (green << 8) | blue;
+                outputImg.setRGB(x, y, newColor);
             }
         }
-
-    return outputImg;
+        return outputImg;
     }
-
 }
 
