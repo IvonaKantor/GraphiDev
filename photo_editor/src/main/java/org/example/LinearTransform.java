b64976746852ab4c55d949917793d406e4f26fb0
ivona <ik87056@student.uwb.edu.pl>
2025-04-12 12:16:19 +0200

    first methode for brighten an image

--- photo_editor/src/main/java/org/example/LinearTransform.java
+++ photo_editor/src/main/java/org/example/LinearTransform.java
@@ -7,10 +7,11 @@ import java.io.IOException;
 
 public class LinearTransform {
 
-    public static BufferedImage brighten(BufferedImage image, int amount) {
-        int width = image.getWidth();
-        int height = image.getHeight();
-        BufferedImage newImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+    public static BufferedImage brighten(BufferedImage inputImg, int amount) {
+        int width = inputImg.getWidth();
+        int height = inputImg.getHeight();
+        BufferedImage outputImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
         return null;
     }
 }
