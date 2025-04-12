025516d50f806447b6e71928388a6c993c860e7c
ivona <ik87056@student.uwb.edu.pl>
2025-04-12 14:24:58 +0200

    updating

--- photo_editor/src/main/java/org/example/LinearTransform.java
+++ photo_editor/src/main/java/org/example/LinearTransform.java
@@ -7,11 +7,11 @@ import java.io.IOException;
 
 public class LinearTransform {
 
-    public static BufferedImage brighten(BufferedImage inputImg, int amount) {
+    public static BufferedImage brighten(BufferedImage inputImg, double amount) {
         int width = inputImg.getWidth();
         int height = inputImg.getHeight();
         BufferedImage outputImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
-        return null;
+        return outputImg;
     }
 }
