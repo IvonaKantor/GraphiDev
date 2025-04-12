0ff0afb5529599bcddb18f3872086e7602302d4a
ivona <ik87056@student.uwb.edu.pl>
2025-04-12 12:27:58 +0200

    second class for power transformation of an image

--- photo_editor/src/main/java/org/example/PowerTransform.java
+++ photo_editor/src/main/java/org/example/PowerTransform.java
@@ -2,4 +2,17 @@ package org.example;
 import java.awt.image.BufferedImage;
 
 public class PowerTransform {
+    public BufferedImage brighten(BufferedImage inputImg, double amount) {
+
+        if(inputImg == null)
+            return null;
+
+        int width = inputImg.getWidth();
+        int height = inputImg.getHeight();
+        BufferedImage outputImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+    return outputImg;
+    }
+
 }
+
