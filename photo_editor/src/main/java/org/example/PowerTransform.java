2271f54da2785959e72b8c42bbb09fb9a62b6d7a
ivona <ik87056@student.uwb.edu.pl>
2025-04-14 22:55:46 +0200

    getting image parameters and checking for null

--- photo_editor/src/main/java/org/example/PowerTransform.java
+++ photo_editor/src/main/java/org/example/PowerTransform.java
@@ -28,5 +28,14 @@ public class PowerTransform {
     }
 
     public BufferedImage darken(BufferedImage inputImg, double amount) {
+        if (inputImg == null) {
+            return null;
+        }
+        int width = inputImg.getWidth();
+        int height = inputImg.getHeight();
+        BufferedImage outputImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+        return outputImg;
     }
+}
 
