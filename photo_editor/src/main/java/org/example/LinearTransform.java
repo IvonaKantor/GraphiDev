eee342970e32e52c794c21d973e1506a5d90db64
ivona <ik87056@student.uwb.edu.pl>
2025-04-23 22:05:17 +0200

    added checking output image of null value

--- photo_editor/src/main/java/org/example/LinearTransform.java
+++ photo_editor/src/main/java/org/example/LinearTransform.java
@@ -1,13 +1,15 @@
 package org.example;
 
-import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
 public class LinearTransform {
 
-    public static BufferedImage brighten(BufferedImage inputImg, double amount) {
+    public static BufferedImage brighten(BufferedImage inputImg, double amount, int i) {
+
+        if (inputImg == null) {
+            return null;
+        }
+
         int width = inputImg.getWidth();
         int height = inputImg.getHeight();
         BufferedImage outputImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
