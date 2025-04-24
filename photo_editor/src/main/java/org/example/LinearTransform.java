f4cb0b0a23aa0dbfa9868612adee0889c1b00d69
ivona <ik87056@student.uwb.edu.pl>
2025-04-24 23:32:32 +0200

    using color class to work with rgb values in negative methode

--- photo_editor/src/main/java/org/example/LinearTransform.java
+++ photo_editor/src/main/java/org/example/LinearTransform.java
@@ -48,23 +48,17 @@ public class LinearTransform {
     }
 
     public BufferedImage negative(BufferedImage inputImg) {
-        if (inputImg == null) {
-            return null;
-        }
         int width = inputImg.getWidth();
         int height = inputImg.getHeight();
-        BufferedImage outputImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        BufferedImage outputImage = new BufferedImage(width, height, inputImg.getType());
 
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                int rgb = inputImg.getRGB(x, y);
-                int alpha = (rgb >> 24) & 0xFF;
-                int red = 255 - ((rgb >> 16) & 0xFF);
-                int green = 255 - ((rgb >> 8) & 0xFF);
-                int blue = 255 - (rgb & 0xFF);
-
-                int newRgb = (alpha << 24) | (red << 16) | (green << 8) | blue;
-                outputImage.setRGB(x, y, newRgb);
+                Color color = new Color(inputImg.getRGB(x, y));
+                int red = 255 - color.getRed();
+                int green = 255 - color.getGreen();
+                int blue = 255 - color.getBlue();
+                outputImage.setRGB(x, y, new Color(red, green, blue).getRGB());
             }
         }
         return outputImage;
