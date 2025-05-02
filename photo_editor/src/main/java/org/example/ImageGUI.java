681fdfc3d67f64f8a6344354e7ea525871e8b038
ivona <ik87056@student.uwb.edu.pl>
2025-05-02 23:21:23 +0200

    added image panel

--- photo_editor/src/main/java/org/example/ImageGUI.java
+++ photo_editor/src/main/java/org/example/ImageGUI.java
@@ -9,5 +9,17 @@ public class ImageGUI extends JFrame {
         setSize(1200, 800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
+
+        ImageOperations imageOperations = new ImageOperations();
+        ImagePanel imagePanel = new ImagePanel();
+        ControlPanel controlPanel = new ControlPanel(imageOperations, imagePanel);
+
+        add(controlPanel, BorderLayout.NORTH);
+
+        JScrollPane scrollPane = new JScrollPane(imagePanel);
+        scrollPane.setPreferredSize(new Dimension(1200, 700));
+        add(scrollPane, BorderLayout.CENTER);
+
+        setLocationRelativeTo(null);
     }
 }
