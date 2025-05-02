28606ac9411e3eee138865a881648a8731862e8a
ivona <ik87056@student.uwb.edu.pl>
2025-05-02 23:14:38 +0200

    added size and title of a frame

--- photo_editor/src/main/java/org/example/ImageGUI.java
+++ photo_editor/src/main/java/org/example/ImageGUI.java
@@ -3,5 +3,9 @@ package org.example;
 import javax.swing.*;
 
 public class ImageGUI extends JFrame {
-
+    public ImageGUI() {
+        setTitle("Photo Editor");
+        setSize(1200, 800);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
