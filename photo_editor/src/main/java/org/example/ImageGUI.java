058bc3c112adaeb64b188d3c617dc5eb9c887ee4
ivona <ik87056@student.uwb.edu.pl>
2025-05-02 23:18:29 +0200

    updating

--- photo_editor/src/main/java/org/example/ImageGUI.java
+++ photo_editor/src/main/java/org/example/ImageGUI.java
@@ -1,11 +1,13 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class ImageGUI extends JFrame {
     public ImageGUI() {
         setTitle("Photo Editor");
         setSize(1200, 800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
     }
 }
