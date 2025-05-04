1959ae6087370d1f8cb3717dae17b3c0a0338bc0
ivona <ik87056@student.uwb.edu.pl>
2025-05-04 23:04:00 +0200

    completed version of app class

--- photo_editor/src/main/java/org/example/App.java
+++ photo_editor/src/main/java/org/example/App.java
@@ -1,6 +1,12 @@
 package org.example;
 
+import javax.swing.*;
+
 public class App {
     public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            ImageGUI gui = new ImageGUI();
+            gui.setVisible(true);
+        });
     }
 }
