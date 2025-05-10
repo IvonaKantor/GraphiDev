1dca4fd52146a40e5b519ea00b0bc5b95ea2e8ce
ivona <ik87056@student.uwb.edu.pl>
2025-05-10 15:40:13 +0200

    calculating frequency of each color in the image

--- photo_editor/src/main/java/org/example/HistogramOperations.java
+++ photo_editor/src/main/java/org/example/HistogramOperations.java
@@ -9,5 +9,18 @@ public class HistogramOperations {
         int[] redHist = new int[256];
         int[] greenHist = new int[256];
         int[] blueHist = new int[256];
+
+        for (int y = 0; y < original.getHeight(); y++) {
+            for (int x = 0; x < original.getWidth(); x++) {
+                Color color = new Color(original.getRGB(x, y));
+                redHist[color.getRed()]++;
+                greenHist[color.getGreen()]++;
+                blueHist[color.getBlue()]++;
+            }
+        }
+
+        int maxRed = Arrays.stream(redHist).max().getAsInt();
+        int maxGreen = Arrays.stream(greenHist).max().getAsInt();
+        int maxBlue = Arrays.stream(blueHist).max().getAsInt();
     }
 }
